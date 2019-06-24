157589944d7322dd74188b567e2b8d36a0e2f0a4
Artem Kozachenko <kozachenko.artem@gmail.com>
2019-06-24 22:45:54 +0300

    update iron-gcd

--- iron-gcd/src/main.rs
+++ iron-gcd/src/main.rs
@@ -1,7 +1,33 @@
+extern crate iron;
+#[macro_use]extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+
 use std::io::Write;
 use std::str::FromStr;
 
 fn main() {
+   println!("Serving on http://localhost:3000...");
+   //Iron::new(get_form).http("localhost:3000").unwrap();
+}
+
+fn get_form(_request: &mut &Request) -> IronResult<Response> {
+   let mut response = Response::new();
+   response.set_mut(status::Ok);
+   response.set_mut(mime!(Text/Html; Charset=Utf8));
+   response.set_mut(r#"
+	<title> GCD calculator </title>
+	<form action="/gcd" method="post">
+	    <input type="text" name="n"/>
+	    <input type="text" name="n"/>
+            <button type="submit">Compute</button>
+        </form>
+   "#);
+   Ok(response)
+}
+
+fn x() {
     let mut numbers = Vec::new();
     for arg in std::env::args().skip(1) {
         numbers.push(u64::from_str(&arg)
