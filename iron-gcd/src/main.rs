ff2b69b70da20e377182e2bade4b14615a0ec68a
Artem Kozachenko <artem.kozachenko@ring.com>
2019-06-25 15:30:27 +0300

    finish iron-gcd

--- iron-gcd/src/main.rs
+++ iron-gcd/src/main.rs
@@ -1,18 +1,23 @@
 extern crate iron;
+extern crate router;
+extern crate urlencoded;
 #[macro_use]extern crate mime;
 
+use router::Router;
 use iron::prelude::*;
 use iron::status;
-
-use std::io::Write;
+use urlencoded::UrlEncodedBody;
 use std::str::FromStr;
 
 fn main() {
-   println!("Serving on http://localhost:3000...");
-   //Iron::new(get_form).http("localhost:3000").unwrap();
+   let mut router = Router::new();
+   router.get("/", get_form, "root");
+   router.post("/gcd", post_gcd, "gcd");
+   println!("Server started on 3000");
+   Iron::new(router).http("localhost:3000").unwrap();
 }
 
-fn get_form(_request: &mut &Request) -> IronResult<Response> {
+fn get_form(_request: &mut Request) -> IronResult<Response> {
    let mut response = Response::new();
    response.set_mut(status::Ok);
    response.set_mut(mime!(Text/Html; Charset=Utf8));
@@ -27,22 +32,45 @@ fn get_form(_request: &mut &Request) -> IronResult<Response> {
    Ok(response)
 }
 
-fn x() {
+fn post_gcd(request: &mut Request) -> IronResult<Response> {
+   let mut response = Response::new();
+   let form_data = match request.get_ref::<UrlEncodedBody>() {
+      Err(e) => {
+           response.set_mut(status::BadRequest);
+           response.set_mut(format!("Error parsing form data : {:?}\n", e));
+           return Ok(response);
+      }
+      Ok(map) => map
+   };
+   let unparsed_number = match form_data.get("n") {
+        None => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Form data has no n "));
+            return Ok(response);
+        }
+        Some(nums) => nums
+   };
+
     let mut numbers = Vec::new();
-    for arg in std::env::args().skip(1) {
-        numbers.push(u64::from_str(&arg)
-                    .expect("error parsing argument"));
+    for unparsed in unparsed_number {
+        match u64::from_str(&unparsed) {
+            Err(_) => {
+                response.set_mut(status::BadRequest);
+                response.set_mut(format!("Value for n parameter is not a number : {:?}", unparsed));
+                return Ok(response);
+            }
+            Ok(n) => { numbers.push(n); }
+        }
     }
-    if numbers.len() == 0 {
-	writeln!(std::io::stderr(), "Usage: gcd NUMBER...").unwrap();
-	std::process::exit(1);
-    }
+
     let mut d = numbers[0];
     for m in &numbers[1..] {
-	d = gcd(d, *m);
+        d = gcd(d, *m);
     }
-    println!("The greatest common devision of {:?} is {}", numbers, d);
-
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(format!("The greatest common devision of <b>{:?}</b> is {}\n", numbers, d));
+    Ok(response)
 }
 
 fn gcd(mut n: u64, mut m:u64) -> u64 {
@@ -59,7 +87,6 @@ fn gcd(mut n: u64, mut m:u64) -> u64 {
 }
 
 #[test]
-
 fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
     assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
