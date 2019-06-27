2bbd7aa6d38ccce7a1541af727d4122c693ec491
Artem Kozachenko <artem.kozachenko@ring.com>
2019-06-27 10:22:48 +0300

    finish mendelbrot forking in one thread

--- mendelbrot/src/main.rs
+++ mendelbrot/src/main.rs
@@ -1,7 +1,12 @@
 extern crate num;
+extern crate image;
+
 use num::Complex;
 use std::str::FromStr;
-
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+use std::io::Write;
 
 /// Пытается определить, принадлежэит ли `с` множеству Мендельброта, ограничившись
 /// `limit` итерациями.
@@ -67,4 +72,64 @@ fn test_parse_complex() {
     assert_eq!(parse_complex(",-0.0654"), None);
 }
 
-fn main(){}
+
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>) -> Complex<f64> {
+    let (width, height) = (lower_right.re - upper_left.re, upper_left.im -lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100),  (25, 75),
+                Complex {re: -1.0, im: 1.0},
+                Complex {re: 1.0, im: -1.0}),
+                Complex {re: -0.5, im: -0.5});
+}
+
+
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          upper_left: Complex<f64>,
+          lower_right: Complex<f64>) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            };
+        }
+    }
+}
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
+}
+
+fn main(){
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), "Пример: {} mendel.png 500x400 -2,1 1,-1", args[0]).unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("Ошибка при разборе размеров изображения");
+    let upper_left = parse_complex(&args[3]).expect("Ошибка при разборе левого верхого угла");
+    let lower_right = parse_complex(&args[4]).expect("Ошибка при разборе правого угла");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    render(&mut pixels, bounds, upper_left, lower_right);
+    write_image(&args[1], &pixels, bounds).expect("Ошибка при записи в файл");
+
+}
